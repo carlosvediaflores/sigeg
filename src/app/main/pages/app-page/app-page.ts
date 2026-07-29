b71889c99b2679ae0e8db251bda0636e9882f1b6
Carlos Rudy Vedia Flores <carlosvf782@gmail.com>
2026-07-28 23:15:02 -0400

    para casa2

--- src/app/main/pages/app-page/app-page.ts
+++ src/app/main/pages/app-page/app-page.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '@auth/services/auth.service';
 
 @Component({
   selector: 'app-app-page',
@@ -8,4 +9,8 @@ import { RouterLink } from '@angular/router';
   styleUrl: './app-page.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppPage {}
+export class AppPage {
+  authService = inject(AuthService);
+
+  user = computed(() => this.authService.user());
+}
